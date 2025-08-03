d89da58833c1f627ed0de518bde67aff6b597d9e
RiyadhCode <muhammadriyadh796@gmail.com>
2025-08-03 17:50:40 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const nama = "muhammad riyadh";
+let usia = 50;
+
+let biodata = document.getElementById("biodata");
+
+function gene() {
+    let generasi;
+
+    if (usia > 10 && usia < 18) { 
+         generasi = "generasi remaja";
+    } 
+    else if (usia > 18 && usia < 30) {
+        generasi = "generasi dewasa";
+    }
+    else if (usia > 30) {
+        generasi = "generasi orang tua";
+    }
+    else if (usia > 2 && usia < 10) {
+        generasi = "generasi anak anak";
+    }
+    else {
+       generasi = "generasi bayi wkwk";
+    }
+
+    return biodata.innerHTML = generasi;
+}
+
+console.log(nama); 
+console.log(usia);
+gene();
+
+
